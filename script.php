00b4b6c49ca6a73e63c1435d5417111017d5b325
DevTest <115026138+adrien-pago@users.noreply.github.com>
2023-11-17 22:11:50 +0100

    Update script.php

--- script.php
+++ script.php
@@ -1,28 +1,30 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php'; // Charge automatiquement les classes nécessaires.
+require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Club\Admin; // Utilisation des espaces de noms pour organiser les classes.
+use App\Club\Admin;
 use App\Club\Regular;
 use App\Club\CachedMember;
+use App\Drinks\Alcool;
 use App\Level;
 use App\User;
 
-echo 'Member count is : ' . Regular::count() . PHP_EOL; // Appelle une méthode statique.
+echo 'Member count is : ' . Regular::count() . PHP_EOL;
 
-$member1 = new Regular(new User('Ad'), 'member1', 'member1', 11); // Création d'instances de classes.
+$member1 = new Regular(new User('Ad'), 'member1', 'member1', 11);
 $member2 = new Regular(new User('Lou'), 'member2', 'member2', 43);
 $member3 = new Regular(new User('Pl'), 'member3', 'member3', 61);
 
 echo 'Member count is : ' . Regular::count() . PHP_EOL;
 
-// Utilisation de l'enrobage et de la composition pour étendre les fonctionnalités.
 $admin1 = new CachedMember(new Admin(new Regular(new User('Po'), 'admin1', 'admin1', 12), Level::from('superadmin')));
 echo 'Admin is ' . $admin1 . PHP_EOL;
 
-unset($member2); // Déréférencement d'une instance.
+unset($member2);
 
 echo 'Member count is : ' . Regular::count() . PHP_EOL;
 
-$admin1->auth('plop', 'plop'); // Appel de méthodes sur les objets.
+$member1->drink(new Alcool());
+
+$admin1->auth('plop', 'plop');
 $member1->auth('plop', 'plop');
